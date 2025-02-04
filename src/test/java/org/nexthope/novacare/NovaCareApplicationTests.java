bc47598d3e4c0093456050d9d833b347b1cefdf3
aminhl <amin.hlel@esprit.tn>
2025-02-04 20:34:54 +0100

    init commit

---
+++ src/test/java/org/nexthope/novacare/NovaCareApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nexthope.novacare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
