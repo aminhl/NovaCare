c808fea84ac0e26d1a9d1b8f86e32b6ee28a4558
aminhl <amin.hlel@esprit.tn>
2025-02-04 21:40:07 +0100

    feat: user modeling

--- src/main/java/org/nexthope/novacare/NovaCareApplication.java
+++ src/main/java/org/nexthope/novacare/NovaCareApplication.java
@@ -2,8 +2,10 @@ package org.nexthope.novacare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class NovaCareApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/org/nexthope/novacare/user/User.java
@@ -0,0 +1,42 @@
+package org.nexthope.novacare.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column(name = "user_id", nullable = false)
+    private String id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstname;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastname;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+}
